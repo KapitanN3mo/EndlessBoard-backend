17ddbefd00a26f45515c2a6c6fe2786647c4d99b
dogsdev <admin@dogsdev.ru>
2024-01-22 20:40:15 +0300

    add ReactionListId.

--- EndlessBoard-backend/classes/ReactionClass.cs
+++ EndlessBoard-backend/classes/ReactionClass.cs
@@ -12,6 +12,8 @@ namespace EndlessBoard_backend.classes
 
         [Required(ErrorMessage = "Text обязательно для заполнения")]
         public string Text { get; set; }
+        public int ReactionListId { get; set; }
+        public ReactionList ReactionList { get; set; }
 
     }
 }
